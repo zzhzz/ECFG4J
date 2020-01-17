3797af0d84030ad05762fa080138750de3d02c04
zzhzz <zhaozhehao@outlook.com>
2020-01-17 21:50:04 +0800

    first

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import soot.Transform;
+import soot.PackManager;
+
+public class Main {
+    public static void main(String[] args) {
+        System.err.println("Transform Java class file into extend-cfg");
+
+    }
+}
---
+++ src/main/java/WholeProgramTransformer.java
@@ -0,0 +1,10 @@
+import soot.SceneTransformer;
+
+import java.util.Map;
+
+public class WholeProgramTransformer extends SceneTransformer {
+
+    protected void internalTransform(String s, Map<String, String> map) {
+
+    }
+}
