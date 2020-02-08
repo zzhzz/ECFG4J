adcdd3fce4a75438dfb2429f3ecabb5d1fe49d8a
zzhzz <zhaozhehao@outlook.com>
2020-02-08 23:00:47 +0800

    work

--- src/main/java/ProgramTransformer.java
+++ src/main/java/ProgramTransformer.java
@@ -1,13 +1,11 @@
 import org.javatuples.Pair;
 import soot.*;
-import soot.jimple.*;
-import soot.jimple.toolkits.callgraph.ReachableMethods;
-import soot.util.Chain;
-import soot.util.queue.QueueReader;
 
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
+import static soot.Scene.v;
 
 
 public class ProgramTransformer extends SceneTransformer {
@@ -15,19 +13,29 @@ public class ProgramTransformer extends SceneTransformer {
     protected void internalTransform(String s, Map<String, String> map) {
         List<Pair<String, String>> dataList = DataManager.getInstance().getClassLabelList();
 
+        int c = 0;
         for(Pair<String, String> dataItem: dataList){
             ExtendCFGList.getInstance().clear();
             String class_name = dataItem.getValue0(), label = dataItem.getValue1();
+            System.out.println("Process of " + class_name);
             SootClass claz = Scene.v().getSootClass(class_name);
             Iterator<SootMethod>  sootMethodIterator = claz.methodIterator();
             while(sootMethodIterator.hasNext()){
                 SootMethod method = sootMethodIterator.next();
-                if(method.isConcrete()) {
+                if(method.hasActiveBody()) {
+                    MethodUtils.process_method(method.getActiveBody());
+                } else if(method.isConcrete()){
                     MethodUtils.process_method(method.retrieveActiveBody());
                 }
             }
-            DataManager.getInstance().saveToJsonFile(class_name, ExtendCFGList.getInstance().extract(label));
+            System.out.println("function numbers: " + ExtendCFGList.getInstance().size());
+            if(ExtendCFGList.getInstance().size() > 0) {
+                DataManager.getInstance().saveToJsonFile(class_name, ExtendCFGList.getInstance().extract(label));
+            } else {
+                c++;
+            }
         }
+        System.out.println(c + " class failed!");
     }
 
 
